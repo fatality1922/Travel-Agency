e75ff338e0a9e0aaabaeeab0abd361e017c7c903
Jan Kowalski <fatality1922@gmail.com>
2021-02-17 18:30:27 +0100

    fix trips filtering by tags, duration and string, descending order

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -1,32 +1,38 @@
-// import React from 'react';
-// import styles from './OrderOption.scss';
-// import PropTypes from 'prop-types';
-// import { formatPrice } from '../../../utils/formatPrice';
-// import Icon from './../../common/Icon/Icon';
+import React from 'react';
+import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
+import { formatPrice } from '../../../utils/formatPrice';
+import Icon from './../../common/Icon/Icon';
 
 
-// const OrderOptionIcons = ({values, required, currentValue, setOptionValue, }) => (
+const OrderOptionIcons = ({ values, required, currentValue, setOptionValue }) => (
 
-//   <div className={styles.component}>
+  <div className={styles.component}>
 
-//     {required ? false : (
-//       <div value=''>
-//         <Icon name={'times-circle'} /> none
-//       </div>
-//     )}
+    {required ? '' : (
+      <div className={styles.icon} onClick={() => setOptionValue('')} value="">
+        <Icon className={styles.icon} name={'times-circle'} /> none
+      </div>
+    )}
 
-//     {values.map(value => (
-//       <div className={styles.icon} key={value.id} value={value.id}>{value.name} ({formatPrice(value.price)})</div>
-//     ))}
-//   </div>
+    {values.map((value) => (
+      <div className={styles.icon && currentValue === value.id ? styles.iconActive : ''}
+        key={value.id}
+        onClick={() => setOptionValue(value.id)}>
 
-// );
+        <Icon name={value.icon} />
+        {value.name}({formatPrice(value.price)})
+      </div>
+    ))}
+  </div>
 
-// OrderOptionIcons.propTypes = {
-//   values: PropTypes.array,
-//   required: PropTypes.bool,
-//   currentValue: PropTypes.string,
-//   setOptionValue: PropTypes.func,
-// };
+);
 
-// export default OrderOptionIcons;
+OrderOptionIcons.propTypes = {
+  values: PropTypes.array,
+  required: PropTypes.bool,
+  currentValue: PropTypes.string,
+  setOptionValue: PropTypes.func,
+};
+
+export default OrderOptionIcons;
--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -1,42 +1,54 @@
 /* SELECTORS */
 
-export const getAllTrips = ({trips}) => trips;
+export const getAllTrips = ({ trips }) => trips;
 
-export const getFilteredTrips = ({trips, filters}) => {
+export const getFilteredTrips = ({ trips, filters }) => {
   let output = trips;
 
   // filter by search phrase
-  if(filters.searchPhrase){
+  if (filters.searchPhrase) {
     const pattern = new RegExp(filters.searchPhrase, 'i');
     output = output.filter(trip => pattern.test(trip.name));
   }
 
   // TODO - filter by duration
+  if (filters.duration) {
+    const minDuration = filters.duration.from;
+    const maxDuration = filters.duration.to;
 
+    output = output.filter((trip) => trip.days >= minDuration && trip.days <= maxDuration);
+  }
   // TODO - filter by tags
-
+  if (filters.tags.length > 0) {
+    output = output.filter(trip => filters.tags.every((tag) => trip.tags.includes(tag)));
+  }
   // TODO - sort by cost descending (most expensive goes first)
-
+  output = output.sort((tripA, tripB) => {
+    const cost1 = parseFloat(tripA.cost.replace('$', '').replace(',', ''));
+    const cost2 = parseFloat(tripB.cost.replace('$', '').replace(',', ''));
+    console.log(cost2);
+    return cost2 - cost1;
+  });
   return output;
 };
 
-export const getTripById = ({trips}, tripId) => {
+export const getTripById = ({ trips }, tripId) => {
   const filtered = trips.filter((trip) => trip.id == tripId);
 
 
   // TODO - filter trips by tripId
 
   console.log('filtering trips by tripId:', tripId, filtered);
-  return filtered.length ? filtered[0] : {error: true};
+  return filtered.length ? filtered[0] : { error: true };
 };
 
-export const getTripsForCountry = ({trips}, countryCode) => {
+export const getTripsForCountry = ({ trips }, countryCode) => {
   const filtered = trips.filter((trip) => trip.country.code == countryCode);
 
   // TODO - filter trips by countryCode
 
   console.log('filtering trips by countryCode:', countryCode, filtered);
-  return filtered.length ? filtered : [{error: true}];
+  return filtered.length ? filtered : [{ error: true }];
 };
 
 /* ACTIONS */
